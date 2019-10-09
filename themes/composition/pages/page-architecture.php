08f9e9ef1ddd100b3f6800137b3ac9693a418f92
Aloisio Alessandro <alessaloisio@gmail.com>
2019-10-09 16:58:17 +0200

    diaporama structure

--- themes/composition/pages/page-architecture.php
+++ themes/composition/pages/page-architecture.php
@@ -2,34 +2,37 @@
 
 <?php get_header(); ?>
 
-
-
 <div class="content">
   <div class="block-black"></div>
   <div class="center">
     <section class="container-diaporama">
+
       <h1>Prendre soin de votre schéma de vie.</h1>
+        
       <div class="content-diaporama">
         <!-- Permet de faire un query pour récupérer les projets -->
         <!-- https://codex.wordpress.org/the_loop => Multiple Loops Example 2 -->
         <?php query_posts( 'category_name=projets' ); ?>
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="posts">
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <!-- début d'un bloc post -->
-          <div class="post">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <small><?php the_time('F jS, Y'); ?></small>
-            <div class="entry">
-              <?php the_content(); ?>
+            <!-- début d'un bloc post -->
+            <div class="post">
+              <?php the_post_thumbnail(); ?>
+              <div class="info-project">
+                <div class="info-content">
+                  <h2 class="title"><?php the_title(); ?> - <?php the_time('Y'); ?></h2>
+                  <a class="link" href="<?php the_permalink(); ?>">Voir le projet</a>
+                </div>
+              </div>
             </div>
-            <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
-          </div>
-          <!-- fin d'un bloc post' -->
+            <!-- fin d'un bloc post' -->
 
-        <?php endwhile; else: ?>
-          <p><?php _e('Désolé, nous n\'avons pas de projet à vous présentez pour le moment.'); ?></p>
-        <?php endif; ?>
+          <?php endwhile; else: ?>
+            <p><?php _e('Désolé, nous n\'avons pas de projet à vous présentez pour le moment.'); ?></p>
+          <?php endif; ?>
+        </div>
       </div>
     </section>
     <section class="histoire"></section>
