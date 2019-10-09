2ee977298dad6e0cfb89c5959a4dc6c9d6341d4e
Serge <serge.bayet1975@gmail.com>
2019-10-09 11:16:47 +0200

    add black style & active item

--- themes/composition/header.php
+++ themes/composition/header.php
@@ -10,9 +10,10 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<link rel="icon" type="image/svg" href="<?php echo get_stylesheet_directory_uri()."/assets/svg/logo-small.svg"; ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri()."/assets/css/style.css"; ?>"">
   <title>Composition <?= wp_title( '|', false ); ?></title>
 
   <?php wp_head(); ?>
@@ -20,21 +21,21 @@
 <body>
 	<div id="body-wrapper">
 		<header class="main-header">
-		<div class="container">
+		<div class="container black-style">
 			<a href="#" class="mh-logo">
 
 				<?php $uri = get_stylesheet_directory_uri()."/assets/svg/logo-full.svg"; ?>
 				<img src="<?php echo $uri ?>"/>
 			</a>
-			<nav class="main-nav">
+			<nav class="main-nav black-style">
 				<ul class="main-nav-list">
-					<li class="home">
+					<li class="menu-item">
 						<a href="#">
 							<?php $uri = get_stylesheet_directory_uri()."/assets/svg/home-minimal.svg"; ?>
 							<img src="<?php echo $uri ?>" width="15"/>
 						</a>
 					</li >
-					<li class="menu-item">
+					<li class="menu-item active">
 						<a href="#">Magasin</a>
 					</li>
 					<li class="menu-item">
@@ -50,7 +51,12 @@
 						<a href="#">Contact</a>
 					</li>
 					<li class="wish-list">
-						<a href="#">Wish List</a>
+						<a href="#">Wish List 
+							
+							<?php $uri = get_stylesheet_directory_uri()."/assets/svg/wish.svg"; ?>
+							<img src="<?php echo $uri ?>" width="15"/>
+							
+						</a>
 					</li>
 				</ul>
 			</nav>
