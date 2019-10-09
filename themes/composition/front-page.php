16dbb440c8bbfb78adf55089ce95f18e37bf9597
Dumont Augustin <augustindmt@gmail.com>
2019-10-09 09:10:00 +0200

    add init acf front-page + SASS

--- themes/composition/front-page.php
+++ themes/composition/front-page.php
@@ -1,5 +1,34 @@
 <?php get_header(); ?>
 
 <h1>Front Page</h1>
+<div class="dispo_imgs">
+    <img class="image" src="<?php echo get_field('bandeau_image_g')['sizes']['medium_large']; ?>">
+    <img class="image" src="<?php echo get_field('bandeau_image_d')['sizes']['medium_large']; ?>">
+</div>
+<h1 class="title"><?php the_field('bandeau_titre_g') ?></h1>
+<h3 class="sous-titre"><?php the_field('bandeau_survol_g')['titre'] ?></h3>
+
+
+<?php 
+  $group = get_field('bandeau_bouton');
+
+  if($group['title']) :
+
+  $url = $group['url'];
+  $target = 'target="_blank" rel="noopener"';
+
+  if( $group['choice'] == 'pagelink' ) {
+      $url = $group['page_link'];
+      $target = '';
+  } ?>
+
+  <a class="lien" href="<?php echo $url ?>" <?php echo $target; ?> >
+      <div class="icon-container">
+        <img class="icon" href="<?php echo "./composition/wp-content/themes/composition/assets/svg/fleche-droite.svg"; ?>">
+      </div>
+      <p><?php echo $group['title']; ?></p>
+  </a>
+
+  <?php endif; ?>
 
 <?php get_footer(); ?>  
--- themes/composition/header.php
+++ themes/composition/header.php
@@ -10,6 +10,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
+  <link rel ="stylesheet" type="text/css" href="./assets/css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
