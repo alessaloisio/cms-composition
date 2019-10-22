6adbf6ccc9eddcd5e6d076039d00b3b627373bbe
Dumont Augustin <augustindmt@gmail.com>
2019-10-22 12:02:58 +0200

    add SASS frontPage before responsive

--- themes/composition/front-page.php
+++ themes/composition/front-page.php
@@ -1,14 +1,11 @@
 <?php get_header(); ?>
 
-<div class="center">
 
-<h1>Front Page</h1>
-
+ <!---------------------------------------------------------------------------BANDEAU--------------------------------------------------------------------->
 <section class="section_bandeau">
-   <!---------------------------------------------------------------------------BANDEAU--------------------------------------------------------------------->
   <!--COL GAUCHE BANDEAU-->
   <div class="bandeau_col-g">
-    <img class="bandeau_img" src="<?php echo get_field('bandeau_image_g')['sizes']['medium_large']; ?>">
+    <div class="bandeau_img" style="background: url(<?php echo get_field('bandeau_image_g')['sizes']['medium_large']; ?>) no-repeat center center;"></div>
     <div class="bandeau_white-container bandeau_block">
       <h1 class="title"><?php the_field('bandeau_titre_g') ?></h1>
     </div>
@@ -36,18 +33,22 @@
         </div>
       </a>
       <?php endif; ?>
-    </div>
+      </div>
+    
   </div>
 
 
   <!--COL DROITE BANDEAU-->
   <div class="bandeau_col-d">
-    <img class="bandeau_img" src="<?php echo get_field('bandeau_image_d')['sizes']['medium_large']; ?>">
+    <div class="bandeau_img" style="background: url(<?php echo get_field('bandeau_image_d')['sizes']['medium_large']; ?>) no-repeat center center;"></div>
+
+    
     <div class="bandeau_black-container bandeau_block">
       <h1 class="title"><?php the_field('bandeau_titre_d') ?></h1>
     </div>
     <!--HOVER-->
     <div class="bandeau_black-container-hover bandeau_hover">
+    <div class ="bandeau_relative">
       <h3><?php the_field('bandeau_survol_d_titre')['titre'] ?></h3>
       <p><?php the_field('bandeau_survol_d_texte_d')['titre'] ?></p>
       <?php 
@@ -62,21 +63,35 @@
                             $url = $group['page_link'];
                             $target = '';
                         } ?>
-      <a class="dispo_lien dispo_lien_white" href="<?php echo $url ?>" <?php echo $target; ?>>
+      <a class="dispo_lien_white" href="<?php echo $url ?>" <?php echo $target; ?>>
         <p><?php echo $group['titre']; ?></p>
-        <div class="icon-container">
-          <img class="icon" src="<?php echo get_template_directory_uri(); ?>/assets/svg/fleche-droite.svg" width="20">
-        </div>
+        <span class="icon-container-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 15.429 9.286">
+                    <g id="fleche-droite" transform="translate(12.929 8.786) rotate(180)">
+                      <line id="Line_40" data-name="Line 40" x1="13.879" transform="translate(-1.45 4.143)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"/>
+                      <path id="Path_612" data-name="Path 612" d="M4.143,0,0,4.143,4.143,8.286" transform="translate(-2)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"/>
+                    </g>
+                  </svg>
+                </span>
       </a>
       <?php endif; ?>
     </div>
-  </div>
+    <span class="next absolute">
+                <svg xmlns="http://www.w3.org/2000/svg" width="15.429" height="9.286" viewBox="0 0 15.429 9.286">
+                  <g id="fleche-droite" transform="translate(12.929 8.786) rotate(180)">
+                    <line id="Line_40" data-name="Line 40" x1="13.879" transform="translate(-1.45 4.143)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"/>
+                    <path id="Path_612" data-name="Path 612" d="M4.143,0,0,4.143,4.143,8.286" transform="translate(-2)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"/>
+                  </g>
+                </svg>
+              </span>
+    </div>
+   </div>
 </section>
 
 
 
 
-
+<div class="center">
 <!---------------------------------------------------------------------------INTRODUCTION-------------------------------------------------------------------->
 <section class="section_introduction">
   <div class="introduction_col-g">
@@ -109,8 +124,10 @@
       <?php endif; ?>
     </div>
   </div>
-  <div class="col_d">
-    <img class="introduction_img" src="<?php echo get_field('introduction_image')['sizes']['medium_large']; ?>">
+  <div class="introduction_col-d">
+    <div class ="introduction_col-img" >
+    <div class="introduction_img" style="background: url(<?php echo get_field('introduction_image')['sizes']['medium_large']; ?>) no-repeat center center;"></div>
+    </div>
   </div>
 </section>
 
@@ -121,7 +138,7 @@
   <div class="centre_grey-container">
     <div class="centre_white-container">
       <div class="icon-container">
-        <img class="icon" src="<?php echo get_template_directory_uri(); ?>/assets/svg/logo-small.svg" width="20">
+        <img class="icon" src="<?php echo get_template_directory_uri(); ?>/assets/svg/logo-small.svg" width="40">
       </div>
       <p class="centre_texte"><?php the_field('centre_texte')['titre'] ?></p>
       <?php 
@@ -160,6 +177,7 @@
   </div>
 
   <div class="inferieur_col-d">
+    <div class="inferieur_container-white">
     <h3><?php the_field('inferieur_titre')['titre'] ?></h3>
     <p><?php the_field('inferieur_textarea')['titre'] ?></p>
     <?php 
@@ -184,6 +202,7 @@
       </div>
     </a>
     <?php endif; ?>
+    </div>
   </div>
 </section>
 </div>
