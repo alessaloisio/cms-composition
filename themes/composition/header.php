ddd0a9a34c4591cf88f1e170d2495fe3dbb499e1
Aloisio Alessandro <alessaloisio@gmail.com>
2019-10-22 10:37:43 +0200

    add responsive mobile and tablet

--- themes/composition/header.php
+++ themes/composition/header.php
@@ -13,7 +13,6 @@
 	<link rel="icon" type="image/svg" href="<?php echo get_stylesheet_directory_uri()."/assets/svg/logo-small.svg"; ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri()."/assets/css/style.css"; ?>"">
   <title>Composition <?= wp_title( '|', false ); ?></title>
 
 	<?php wp_head(); ?>
