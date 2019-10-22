da55b5d2e15ccb75427203207e74ad85e42504ae
anso2410 <simana.annesophie@gmail.com>
2019-10-22 10:08:06 +0200

    bloglist init

--- themes/composition/index.php
+++ themes/composition/index.php
@@ -2,24 +2,36 @@
 
 <?php get_header(); ?>
 
+<div class="container-bloglist">
+
+<?php query_posts(array(
+    "category_name" => "articles",
+    "posts_per_page" => 6
+)); ?>
+<div class="banner-bloglist">
+ 
+</div>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <!-- Nous voulons exclure les projets -->
-  <?php if (in_category('projets')) continue; ?> 
-
   <!-- début d'un bloc post -->
-  <div class="post">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <small><?php the_time('F jS, Y'); ?></small>
-    <div class="entry">
-      <?php the_content(); ?>
+ 
+    <div class="post" >
+
+      <?php the_post_thumbnail(); ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <small><?php the_time('F jS, Y'); ?></small>
     </div>
-    <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
-  </div>
+    
+   
+    
+
   <!-- fin d'un bloc post' -->
 
 <?php endwhile; else: ?>
   <p><?php _e('Désolé, nous n\'avons pas d\'article à vous proposez pour le moment.'); ?></p>
 <?php endif; ?>
+</div>
+
 
 <?php get_footer(); ?>  
