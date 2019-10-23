d077e58fd0382d21bf64307a9e8f420737fb6475
anso2410 <simana.annesophie@gmail.com>
2019-10-23 10:59:42 +0200

    ajout css et banner

--- themes/composition/index.php
+++ themes/composition/index.php
@@ -1,29 +1,30 @@
 <!-- BLOG PAGE -->
 <?php get_header(); ?>
-<div class="page-content">
-   <div class="block-black index"></div>
-      <?php query_posts(array("category_name" => "articles","posts_per_page" => 6 )); ?>
-      <section>
-      <h1> "Architecte d'intérieur en action"
-      :: after</h1> 
-      </section>
-     
-        <div class="container-bloglist" >
-         
-             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <!-- début d'un bloc post -->
-             <div class="post" class="attachment" >
-                <div class="image"><?php the_post_thumbnail(array(670,500)); ?></div>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <small><?php the_time('F jS, Y'); ?></small>
-              </div>
-            <!-- fin d'un bloc post' -->
-              <?php endwhile; else: ?>
-                <p><?php _e('Désolé, nous n\'avons pas d\'article à vous proposez pour le moment.'); ?></p>
-                  <?php endif; ?>
-          
-      </div>
-</div>
 
+<div class="page-content">
+  <div class="block-black"></div>
+  <div class="block-content center">
+    <section class="container-diaporama bloglist">
+
+      <h1>Architecture d'intérieur en action</h1>
+        
+      <div class="container-bloglist" >
+        <?php query_posts(array("category_name" => "articles","posts_per_page" => 6 )); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <!-- début d'un bloc post -->
+          <div class="post" class="attachment" >
+            <div class="image"><?php the_post_thumbnail(array(670,500)); ?></div>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <small><?php the_time('F jS, Y'); ?></small>
+          </div>
+        <!-- fin d'un bloc post' -->
+        <?php endwhile; else: ?>
+          <p><?php _e('Désolé, nous n\'avons pas d\'article à vous proposez pour le moment.'); ?></p>
+        <?php endif; ?>     
+      </div>
+
+    </section>
+  </div>
+</div>
 
 <?php get_footer(); ?>  
