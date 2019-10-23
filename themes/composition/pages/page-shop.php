61df475ab37f41f811a323724fbae4087f4b9ba0
Aloisio Alessandro <alessaloisio@gmail.com>
2019-10-23 16:20:20 +0200

    add animation group page

--- themes/composition/pages/page-shop.php
+++ themes/composition/pages/page-shop.php
@@ -6,9 +6,9 @@
   <div class="block-black"></div>
   <div class="block-content">
     <section class="container-diaporama">
-      <h1 class="center"><?= get_field('nom_groupe', 'options'); ?></h1>
+      <h1 class="center slide-content-left"><?= get_field('nom_groupe', 'options'); ?></h1>
       <div class="content-diaporama center">
-        <div class="groupe-container">
+        <div class="groupe-container fade-content">
           <img src="<?= get_field('image_groupe', 'options')['url'] ?>" alt="<?= $value['image_groupe']['title'] ?>" />
         </div>
       </div>
@@ -20,23 +20,24 @@
         if(!empty($coders)):
           $i = 2;
           foreach($coders as $coder):
-            $img = '<div class="block img-block">
-                    <img src="'.$coder['image']['url'].'" alt="'.$coder['image']['title'].'" />
-                  </div>';
 
             $odd = $i % 2;
+
+            $img = '<div class="block img-block'. ($odd === 0 ? " slide-content-left" : " slide-content-right") .'">
+                    <img src="'.$coder['image']['url'].'" alt="'.$coder['image']['title'].'" />
+                  </div>';
 
             if($odd === 0) echo $img;
       ?>
 
-        <div class="block">
+        <div class="block<?= ($odd > 0 ? " slide-content-left" : " slide-content-right") ?>">
           <h1><?= $coder['name'] ?></h1>
           <?= $coder['bio'] ?>
         </div>
 
       <?php 
             if($odd > 0) echo $img;
-            
+
             $i++;
           endforeach; 
         endif;
